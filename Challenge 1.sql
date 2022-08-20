3f9ec2a3ef53bb2f333a2a3bffbad266dd8510ed
Snoutband <76590025+Snoutband@users.noreply.github.com>
2022-08-20 12:07:52 -0500

    Update Challenge 1.sql

--- Challenge 1.sql
+++ Challenge 1.sql
@@ -13,4 +13,6 @@ Production.ProductCategory & Production.ProductSubcategory should show nulls whe
 /*
 Data dictonary for Adventure Works DB: 
 https://docs.microsoft.com/en-us/previous-versions/sql/sql-server-2008/ms124438(v=sql.100)?redirectedfrom=MSDN
+SQL Order of Operations: 
+FROM, JOIN, WHERE, GROUP BY, HAVING, SELECT, ORDER BY 
 *\
