280666e2c3db57af791110b98b5a79002a8d9a9f
Snoutband <76590025+Snoutband@users.noreply.github.com>
2022-08-20 11:15:41 -0500

    Update Challenge 1.sql

--- Challenge 1.sql
+++ Challenge 1.sql
@@ -1,1 +1,16 @@
+/* Use Columns: PurchaseOrderID, PurchaseOrderDetailID, OrderQty, UnitPrice, LineTotal, OrderDate
+Requirement for the new “OrderSizeCategory” to be calculated using case:
+  OrderQty > 500, “Large”
+  OrderQty > 50 but <= 500, “Medium”
+  else "Small”
+Shorten product name and subcategory and if they are null return "None".
+Date criteria is the month of December.
+Purchasing.PurchaseOrderDetail & Purchasing.PurchaseOrderHeader should match when joning. 
+Production.Product & Purchasing.PurchaseOrderDetail should match when joning.
+Production.ProductSubcategory & Production.Product should show nulls when joning.
+Production.ProductCategory & Production.ProductSubcategory should show nulls when joning. *\
 
+/*
+Data dictonary for Adventure Works DB: 
+https://docs.microsoft.com/en-us/previous-versions/sql/sql-server-2008/ms124438(v=sql.100)?redirectedfrom=MSDN
+*\
