bc29ca9b090c9a396cb3a1ba99e6918d35accd41
Snoutband <76590025+Snoutband@users.noreply.github.com>
2022-08-20 11:51:15 -0500

    Update Challenge 1.sql

--- Challenge 1.sql
+++ Challenge 1.sql
@@ -1,14 +1,14 @@
 /* Use Columns: PurchaseOrderID, PurchaseOrderDetailID, OrderQty, UnitPrice, LineTotal, OrderDate
-Requirement for the new “OrderSizeCategory” to be calculated using case:
+Requirement for the new “OrderSizeCategory” to be calculated:
   OrderQty > 500, “Large”
   OrderQty > 50 but <= 500, “Medium”
   else "Small”
 Shorten product name and subcategory and if they are null return "None".
-Date criteria is the month of December.
-Purchasing.PurchaseOrderDetail & Purchasing.PurchaseOrderHeader should match when joning. 
-Production.Product & Purchasing.PurchaseOrderDetail should match when joning.
-Production.ProductSubcategory & Production.Product should show nulls when joning.
-Production.ProductCategory & Production.ProductSubcategory should show nulls when joning. *\
+Date criteria is the month of December. MONTH()
+Purchasing.PurchaseOrderDetail & Purchasing.PurchaseOrderHeader should match when joining. (Inner Join)
+Production.Product & Purchasing.PurchaseOrderDetail should match when joining. (Inner Join)
+Production.ProductSubcategory & Production.Product should show nulls when joining. (Left Outer Join)
+Production.ProductCategory & Production.ProductSubcategory should show nulls when joining. (Left Outer Join) *\ 
 
 /*
 Data dictonary for Adventure Works DB: 
