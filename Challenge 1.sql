8add679e6a7fab4f2e902b47ac741aeb7711a9ef
Snoutband <76590025+Snoutband@users.noreply.github.com>
2022-08-26 13:34:57 -0500

    Update Challenge 1.sql

--- Challenge 1.sql
+++ Challenge 1.sql
@@ -1,5 +1,6 @@
 # Summarized Instructions
-/* Use Columns: [Purchasing].[PurchaseOrderDetail].[PurchaseOrderID], [Purchasing].[PurchaseOrderDetail].[PurchaseOrderDetailID], 
+/* 
+Use Columns: [Purchasing].[PurchaseOrderDetail].[PurchaseOrderID], [Purchasing].[PurchaseOrderDetail].[PurchaseOrderDetailID], 
 [Purchasing].[PurchaseOrderDetail].[OrderQty], [Purchasing].[PurchaseOrderDetail].[UnitPrice], 
 [Purchasing].[PurchaseOrderDetail].[LineTotal], [Purchasing].[PurchaseOrderHeader].[OrderDate], [Production].[Product].[Name], 
 [Production].[ProductSubcategory].[Name], [Production].[ProductCategory].[Name]
@@ -12,16 +13,20 @@ Date criteria is the month of December. MONTH()
 Purchasing.PurchaseOrderDetail & Purchasing.PurchaseOrderHeader should match when joining. (Inner Join)
 Production.Product & Purchasing.PurchaseOrderDetail should match when joining. (Inner Join)
 Production.ProductSubcategory & Production.Product should show nulls when joining. (Left Outer Join)
-Production.ProductCategory & Production.ProductSubcategory should show nulls when joining. (Left Outer Join) *\ 
+Production.ProductCategory & Production.ProductSubcategory should show nulls when joining. (Left Outer Join) 
+*\ 
 
+# Layout Diagram
 /*
 Data dictonary for Adventure Works DB: 
 https://docs.microsoft.com/en-us/previous-versions/sql/sql-server-2008/ms124438(v=sql.100)?redirectedfrom=MSDN
+No entity relationship diagram provided.
 SQL Order of Operations: 
 FROM, JOIN, WHERE, GROUP BY, HAVING, SELECT, ORDER BY 
 I assume all columns mentioned in the question need to be in the output.
 *\
 
+-- # Creating the View
 USE AdventureWorks2019;  
 GO  
 
