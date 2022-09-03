a06865fe7f5985394a2adb7ac3087e4863f807cf
Snoutband <76590025+Snoutband@users.noreply.github.com>
2022-09-03 11:20:48 -0500

    Update Challenge 3.sql

--- Challenge 3.sql
+++ Challenge 3.sql
@@ -1,22 +1,33 @@
 -- Decided to skip challenge 2, it only involved UNION.
 -- Summarized Instructions:
 
-/* 
-SELECT  
-[Person].[BusinessEntityID],  
-[Person].[PersonType], 
-Person.Person.  
-  -- A derived column, aliased as “FullName”, that combines the first, last, and middle names from Person.Person
-  -- One space between each of the names. 
-  -- Middle name maybe null 
-  -- ISNULL() will result in an unwanted space 
-  -- Need to account for the middle name being NULL in a CASE statement (try using two separate columns) 
-[Address].[AddressLine1] [Address], 
-[Address].[City], 
-[Address].[PostalCode], 
-[StateProvince].[Name] [State], 
-[CountryRegion].[Name] [Country] 
 
+-- Output
+SELECT  
+	[Bussiness Address] .[BusinessEntityID] AS [Business ID],  
+	[Person].[Person].[PersonType], 
+	CONCAT([Person].[FirstName], ' ', ISNULL([MiddleN],''), ' ', [Person].[Lastname]) AS "Full Name",
+	[Address].[AddressLine1] AS [Address], 
+	[Address].[City], 
+	[Address].[PostalCode], 
+	[State].[Name] AS [State], 
+	[Country].[Name] AS [Country]
+
+-- Joins
+FROM [Person].[Person]
+	INNER JOIN [Person].[BusinessEntityAddress] AS [Bussiness Address] 
+		ON [Person].[Person].[BusinessEntityID] = [Bussiness Address].BusinessEntityID 
+	INNER JOIN [Person].[Address] AS [Address] 
+		ON [Bussiness Address].[AddressID] = [Address].[AddressID]
+	INNER JOIN [Person].[StateProvince] AS [State]
+		ON [Address].[StateProvinceID] = [State].[StateProvinceID]
+	INNER JOIN [Person].[CountryRegion] [Country] 
+		ON [State].[CountryRegionCode] = [Country].[CountryRegionCode]
+
+-- Criteria
+WHERE [Bussiness Address] .[BusinessEntityID] IN (SELECT 
+
+/* 
 -- Filter requirements: 
   -- Person Type = "SP" or postal code begins with 9 and is exactly 5 characters and country is "United states" 
   -- LEFT() and LEN() should be used for the postal code. 
