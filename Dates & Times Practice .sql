946a96e276cdfb785ec12d5b11b2c5e7df6882c4
Snoutband <76590025+Snoutband@users.noreply.github.com>
2022-09-09 09:03:39 -0500

    Update Dates & Times Practice .sql

--- Dates & Times Practice .sql
+++ Dates & Times Practice .sql
@@ -1,1 +1,11 @@
--- Dates and Times
+– Summarized Instructions:
+– Select the difference in days remaining until the end of the month, difference between the current date and the last day of the current month. 
+– Hint provided to accomplish this:
+  – GETDATE() – today's date
+  – Plug in the YEAR() and MONTH() of Today's date into DATEFROMPARTS() to get the first day of the month
+  – DATEADD to add 1 month to the previous step
+  – DATEADD  to subtract 1 day
+  – DATEDIFF to find  the difference between the current date and the end-of-month.
+– Lots of nested functions
+– Recommend using new lines to keep track of parentheses
+– Everything should be dynamic.
