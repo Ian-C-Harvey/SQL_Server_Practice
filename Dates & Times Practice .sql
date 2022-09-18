66483f3adc4f07fe010c9b0e44196651804574de
Snoutband <76590025+Snoutband@users.noreply.github.com>
2022-09-18 06:58:01 -0500

    Update Dates & Times Practice .sql

--- Dates & Times Practice .sql
+++ Dates & Times Practice .sql
@@ -7,7 +7,6 @@
       Plug in the YEAR() and MONTH() of Today's date into DATEFROMPARTS() to get the first day of the month
       DATEADD to add 1 month to the previous step
       DATEADD  to subtract 1 day
-      DATEDIFF to find  the difference between the current date and the end-of-month.
 *\
     
    SELECT 
