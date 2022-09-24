76f6c4eae4f6e8aacfaf3dfdee89924bb216b287
Snoutband <76590025+Snoutband@users.noreply.github.com>
2022-09-24 09:19:12 -0500

    Update 1 Filtering Practice .sql

--- 1 Filtering Practice .sql
+++ 1 Filtering Practice .sql
@@ -11,7 +11,7 @@ Join Person.CountryRegion to Person.StateProvince.
 -- Output
 SELECT  
 [Bussiness Address].[BusinessEntityID] [Business ID],  
-[Person].[Person].[PersonType], 
+[Person].[PersonType], 
 CONCAT([Person].[FirstName], ' ', ISNULL([MiddleName],''), ' ', [Person].[Lastname]) [Full Name],
 [Address].[AddressLine1] [Address], 
 [Address].[City], 
@@ -20,15 +20,16 @@ CONCAT([Person].[FirstName], ' ', ISNULL([MiddleName],''), ' ', [Person].[Lastna
 [Country].[Name] [Country]
 
 -- Joins
-FROM [Person].[Person]
+FROM [Person].[Person] AS [Person]
 INNER JOIN [Person].[BusinessEntityAddress] AS [Bussiness Address] 
-	ON [Person].[Person].[BusinessEntityID] = [Bussiness Address].[BusinessEntityID] 
+	ON [Person].[BusinessEntityID] = [Bussiness Address].[BusinessEntityID] 
 INNER JOIN [Person].[Address] AS [Address] 
 	ON [Bussiness Address].[AddressID] = [Address].[AddressID]
 INNER JOIN [Person].[StateProvince] AS [State]
 	ON [Address].[StateProvinceID] = [State].[StateProvinceID]
-INNER JOIN [Person].[CountryRegion] [Country] 
+INNER JOIN [Person].[CountryRegion] AS [Country] 
 	ON [State].[CountryRegionCode] = [Country].[CountryRegionCode]
+-- Person is the name of a Schema and one of the tables in the database.
 
 -- Criteria with Subquery
 WHERE EXISTS
@@ -43,5 +44,4 @@ FROM [Person].[Person]
 	INNER JOIN [Person].[CountryRegion] [Country] 
 		ON [State].[CountryRegionCode] = [Country].[CountryRegionCode]
 WHERE ([Person].[Person].[PersonType] = 'SP' OR [Address].[PostalCode] LIKE '9%') AND (LEN([Address].[PostalCode]) = 5 AND [Country].[Name] Like 'United%'));
--- All of the joins had to be added so I could use Country in the criteria.
 -- Added subquery for practice.
