6c6132668e4e5a44f49e856f0e7cecb3aba29093
Snoutband <76590025+Snoutband@users.noreply.github.com>
2022-09-24 09:35:19 -0500

    Update 1 Joins Practice .sql

--- 1 Joins Practice .sql
+++ 1 Joins Practice .sql
@@ -1,26 +1,16 @@
 -- Summarized Instructions 
 /* 
 Names of Columns for SELECT are given in the instructions.
-Requirement for the new “OrderSizeCategory” to be calculated: (CASE WHEN)
+Requirement for the new “OrderSizeCategory” to be calculated:
   OrderQty > 500, “Large”
   OrderQty > 50 but <= 500, “Medium”
   else "Small”
-Shorten product name and subcategory and if they are null return "None". ISNULL()
+Shorten product name and subcategory and if they are null return "None".
 Date criteria is the month of December. MONTH()
 Purchasing.PurchaseOrderDetail & Purchasing.PurchaseOrderHeader should match when joining. (Inner Join)
 Production.Product & Purchasing.PurchaseOrderDetail should match when joining. (Inner Join)
 Production.ProductSubcategory & Production.Product should show nulls when joining. (Left Outer Join)
 Production.ProductCategory & Production.ProductSubcategory should show nulls when joining. (Left Outer Join) 
-*/
-
--- Layout
-/*
-Data dictonary for Adventure Works DB: 
-https://docs.microsoft.com/en-us/previous-versions/sql/sql-server-2008/ms124438(v=sql.100)?redirectedfrom=MSDN
-No entity relationship diagram provided.
-SQL Order of Operations: 
-FROM, JOIN, WHERE, GROUP BY, HAVING, SELECT, ORDER BY 
-I assume all columns mentioned in the question need to be in the output.
 */
 
 -- Ensuring the correct database
