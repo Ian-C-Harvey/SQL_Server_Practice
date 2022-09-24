ee7365d85d4a04524da8074b71352fbf7ae82fdd
Snoutband <76590025+Snoutband@users.noreply.github.com>
2022-09-24 09:21:37 -0500

    Update 1 Filtering Practice .sql

--- 1 Filtering Practice .sql
+++ 1 Filtering Practice .sql
@@ -31,17 +31,16 @@ INNER JOIN [Person].[CountryRegion] AS [Country]
 	ON [State].[CountryRegionCode] = [Country].[CountryRegionCode]
 -- Person is the name of a Schema and one of the tables in the database.
 
--- Criteria with Subquery
+-- Criteria with Subquery for Practice
 WHERE EXISTS
 (SELECT * 
-FROM [Person].[Person]
-	INNER JOIN [Person].[BusinessEntityAddress] AS [Bussiness Address] 
-		ON [Person].[Person].[BusinessEntityID] = [Bussiness Address].BusinessEntityID 
-	INNER JOIN [Person].[Address] AS [Address] 
-		ON [Bussiness Address].[AddressID] = [Address].[AddressID]
-	INNER JOIN [Person].[StateProvince] AS [State]
-		ON [Address].[StateProvinceID] = [State].[StateProvinceID]
-	INNER JOIN [Person].[CountryRegion] [Country] 
-		ON [State].[CountryRegionCode] = [Country].[CountryRegionCode]
-WHERE ([Person].[Person].[PersonType] = 'SP' OR [Address].[PostalCode] LIKE '9%') AND (LEN([Address].[PostalCode]) = 5 AND [Country].[Name] Like 'United%'));
--- Added subquery for practice.
+FROM [Person].[Person] AS [Person]
+INNER JOIN [Person].[BusinessEntityAddress] AS [Bussiness Address] 
+	ON [Person].[BusinessEntityID] = [Bussiness Address].[BusinessEntityID] 
+INNER JOIN [Person].[Address] AS [Address] 
+	ON [Bussiness Address].[AddressID] = [Address].[AddressID]
+INNER JOIN [Person].[StateProvince] AS [State]
+	ON [Address].[StateProvinceID] = [State].[StateProvinceID]
+INNER JOIN [Person].[CountryRegion] AS [Country] 
+	ON [State].[CountryRegionCode] = [Country].[CountryRegionCode]
+WHERE ([Person].[PersonType] = 'SP' OR [Address].[PostalCode] LIKE '9%') AND (LEN([Address].[PostalCode]) = 5 AND [Country].[Name] Like 'United%'));
