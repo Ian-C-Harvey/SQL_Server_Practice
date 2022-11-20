52e7a6bf94f6c70345a44a6967d682588a2c4426
Snoutband <76590025+Snoutband@users.noreply.github.com>
2022-11-20 11:43:17 -0600

    Update 2_Detailed_Report_Queries.sql

--- 2_Detailed_Report_Queries.sql
+++ 2_Detailed_Report_Queries.sql
@@ -1,7 +1,17 @@
 -- Pricing
 SELECT
-  SUM(UnitPrice * Quantity) AS TotalPrice, SUM((UnitPrice * Quantity) - ((UnitPrice * Quantity) * Discount)) AS TotalPriceAfterDiscount
+  SUM(UnitPrice * Quantity) AS TotalPrice, 
+  SUM((UnitPrice * Quantity) - ((UnitPrice * Quantity) * Discount)) AS TotalPriceAfterDiscount
 FROM Orders O
-JOIN OrderItems OI 
-  ON O.OrderID = OI.OrderID
+  Inner Join OrderItems OI ON O.OrderID = OI.OrderID
 WHERE O.OrderID = 10250;
+
+Select 
+  C.companyName, 
+  SUM((OI.UnitPrice * OI.Quantity) - ((OI.UnitPrice * OI.Quantity) * OI.Discount)) As TotalPaid
+From Customers C
+  Inner Join Orders O on C.CustomerID = O.CustomerID
+  Inner Join OrderItems OI on O.OrderID = OI.OrderID 
+WHERE O.OrderDate Between '2016-06-01' AND '2016-07-31'
+Group By C.CustomerID, C.CompanyName
+Order By totalPaid DESC;
